dd0b224b2316308312f108d134c88f8c7ed93588
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2023-12-03 13:40:50 +0530

    Drag & Drop test added

--- src/test/java/com/tribe/workshop/appium/tests/dec2023/GestureTests.java
+++ src/test/java/com/tribe/workshop/appium/tests/dec2023/GestureTests.java
@@ -1,5 +1,6 @@
 package com.tribe.workshop.appium.tests.dec2023;
 
+import com.github.javafaker.App;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
@@ -22,6 +23,7 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class GestureTests {
@@ -64,11 +66,10 @@ public class GestureTests {
 
         // start driver
         driver = new AndroidDriver(new URI(appiumService.getUrl().toString()).toURL(), uiAutomator2Options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
         logger.info("Is App Installed : " + driver.isAppInstalled("com.vodqareactnative"));
-        driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"LOG IN\")")).click();
-
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"LOG IN\")")).click();
     }
 
     @Test
@@ -92,6 +93,97 @@ public class GestureTests {
                 .addAction(firstFinger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
 
         driver.perform(Collections.singleton(sliderSequence));
+    }
+
+    @Test
+    public void multiTouchIndividualSliderTest() {
+        // navigate to slider screen
+        findElement(AppiumBy.accessibilityId("slider1")).click();
+
+        // perform gesture
+        WebElement firstSliderElement = findElement(AppiumBy.accessibilityId("slider"));
+        WebElement secondSliderElement = findElement(AppiumBy.accessibilityId("slider1"));
+
+        // center x coordinate for slider - X + (elementWidth/2)
+        int firstSliderCenterX = firstSliderElement.getLocation().getX() + (firstSliderElement.getSize().getWidth() / 2);
+        int secondSliderCenterX = secondSliderElement.getLocation().getX() + (secondSliderElement.getSize().getWidth() / 2);
+        // Create Finger event for first slider
+        PointerInput firstFinger = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+
+        // Create sequence of actions for first slider
+        Sequence firstSliderSequence = new Sequence(firstFinger, 1)
+                .addAction(firstFinger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), getCenterOfElement(firstSliderElement.getLocation(), firstSliderElement.getSize())))
+                .addAction(firstFinger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(new Pause(firstFinger, Duration.ofSeconds(1)))
+                .addAction(firstFinger.createPointerMove(Duration.ofSeconds(2), PointerInput.Origin.viewport(), firstSliderCenterX, firstSliderElement.getLocation().getY()))
+                .addAction(firstFinger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        // Create Finger event for first slider
+        PointerInput secondFinger = new PointerInput(PointerInput.Kind.TOUCH, "finger2");
+
+        // Create sequence of actions for first slider
+        Sequence secondSliderSequence = new Sequence(secondFinger, 1)
+                .addAction(secondFinger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), getCenterOfElement(secondSliderElement.getLocation(), secondSliderElement.getSize())))
+                .addAction(secondFinger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(new Pause(secondFinger, Duration.ofSeconds(1)))
+                .addAction(secondFinger.createPointerMove(Duration.ofMillis(800), PointerInput.Origin.viewport(), secondSliderCenterX, secondSliderElement.getLocation().getY()))
+                .addAction(secondFinger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Arrays.asList(firstSliderSequence, secondSliderSequence));
+    }
+
+    @Test
+    public void multiTouchSliderTest() {
+        // navigate to slider screen
+        findElement(AppiumBy.accessibilityId("slider1")).click();
+
+        // perform gesture
+        WebElement firstSliderElement = findElement(AppiumBy.accessibilityId("slider"));
+        WebElement secondSliderElement = findElement(AppiumBy.accessibilityId("slider1"));
+
+        // center x coordinate for slider - X + (elementWidth/2)
+        int firstSliderCenterX = firstSliderElement.getLocation().getX() + (firstSliderElement.getSize().getWidth() / 2);
+        int secondSliderCenterX = secondSliderElement.getLocation().getX() + (secondSliderElement.getSize().getWidth() / 2);
+        // Create Finger event for first slider
+        PointerInput firstFinger = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+        // Create Finger event for first slider
+        PointerInput secondFinger = new PointerInput(PointerInput.Kind.TOUCH, "finger2");
+
+        // Create sequence of actions for first slider
+        Sequence sliderSequence = new Sequence(firstFinger, 1)
+                .addAction(firstFinger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), getCenterOfElement(firstSliderElement.getLocation(), firstSliderElement.getSize())))
+                .addAction(secondFinger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), getCenterOfElement(secondSliderElement.getLocation(), secondSliderElement.getSize())))
+                .addAction(firstFinger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(secondFinger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(new Pause(firstFinger, Duration.ofSeconds(1)))
+                .addAction(new Pause(secondFinger, Duration.ofSeconds(1)))
+                .addAction(firstFinger.createPointerMove(Duration.ofSeconds(2), PointerInput.Origin.viewport(), firstSliderCenterX, firstSliderElement.getLocation().getY()))
+                .addAction(secondFinger.createPointerMove(Duration.ofMillis(800), PointerInput.Origin.viewport(), secondSliderCenterX, secondSliderElement.getLocation().getY()))
+                .addAction(firstFinger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(secondFinger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Collections.singleton(sliderSequence));
+    }
+
+    @Test
+    public void dragAndDropTest() {
+        findElement(AppiumBy.accessibilityId("dragAndDrop")).click();
+        WebElement dragMeElement = findElement(AppiumBy.accessibilityId("dragMe"));
+        WebElement dropMeElement = findElement(AppiumBy.accessibilityId("dropzone"));
+
+        // find center location of drag Me & Drop Me
+        Point centerXofDragMe = getCenterOfElement(dragMeElement.getLocation(), dragMeElement.getSize());
+        Point centerXofDropMe = getCenterOfElement(dropMeElement.getLocation(), dropMeElement.getSize());
+
+        PointerInput dragMeInput = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+
+        Sequence dragAndDropSequence = new Sequence(dragMeInput, 1)
+                .addAction(dragMeInput.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), centerXofDragMe))
+                .addAction(dragMeInput.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(dragMeInput.createPointerMove(Duration.ofMillis(800), PointerInput.Origin.viewport(), centerXofDropMe))
+                .addAction(dragMeInput.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Collections.singleton(dragAndDropSequence));
     }
 
     private WebElement findElement(By selector) {
