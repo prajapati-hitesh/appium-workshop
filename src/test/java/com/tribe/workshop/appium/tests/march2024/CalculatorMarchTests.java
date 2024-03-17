50e48e362cd3c51e4fa47678c079c30a9e3d846a
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2024-03-17 13:44:45 +0530

    Calculator test file added

--- src/test/java/com/tribe/workshop/appium/tests/march2024/CalculatorMarchTests.java
+++ src/test/java/com/tribe/workshop/appium/tests/march2024/CalculatorMarchTests.java
@@ -1,5 +1,6 @@
 package com.tribe.workshop.appium.tests.march2024;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
@@ -8,10 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 
@@ -54,10 +52,37 @@ public class CalculatorMarchTests {
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
 
+
     private WebElement findElement(By by) {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(by));
     }
 
+    @Test
+    public void addTest() {
+        // First value
+        int a = 10;
+        int b = 20;
+
+        char[] aAsArray = String.valueOf(a).toCharArray(); // [1, 0]
+        char[] bAsArray = String.valueOf(b).toCharArray(); // [2, 0]
+
+        for(char each : aAsArray) {
+            findElement(AppiumBy.accessibilityId(String.valueOf(each))).click();
+        }
+
+        // perform plus
+        findElement(AppiumBy.accessibilityId("plus")).click();
+        for(char each : bAsArray) {
+            findElement(AppiumBy.accessibilityId(String.valueOf(each))).click();
+        }
+
+        // click equals
+        findElement(AppiumBy.accessibilityId("equals")).click();
+
+        // get the value of result
+        WebElement resultElement = findElement(AppiumBy.id("com.google.android.calculator:id/result_final"));
+        System.out.println("Final Result : " + resultElement.getText());
+    }
     @AfterClass
     public void cleanUp() throws InterruptedException {
         Thread.sleep(5000);
